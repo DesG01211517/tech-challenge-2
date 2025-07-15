60e9dca7df3ebf6e5529580fa94d68ab75189024
DesG01211517 <desmongarrison2@gmail.com>
2025-07-14 23:42:00 -0400

    Provisioned EKS cluster with Terraform, updated aws-auth config in progress

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,30 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.40.0"
+      version = ">= 5.40.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">= 2.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.0"
     }
   }
 }
@@ -12,9 +34,6 @@ provider "aws" {
   profile = "TechC2-User"
 }
 
-# ----------------------
-# VPC Module
-# ----------------------
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
@@ -23,61 +42,63 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
 
   tags = {
-    Project = "Tech Challenge 2"
+    Name = "tech-challenge-2-vpc"
   }
 }
 
-# ----------------------
-# EKS Module
-# ----------------------
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "20.37.1"
 
   cluster_name    = "tech-challenge-2-cluster"
-  cluster_version = "1.28"
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
 
-  enable_irsa = true
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t3.medium"]
+  }
 
   eks_managed_node_groups = {
-    node_group = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_types = ["t3.medium"]
+    default = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
     }
   }
 
   tags = {
-    Project = "Tech Challenge 2"
+    Environment = "tech-challenge-2"
+    Terraform   = "true"
   }
 }
 
-# ----------------------
-# ECR Repository for Docker images
-# ----------------------
-resource "aws_ecr_repository" "tech_challenge_repo" {
-  name = "tech-challenge-2-repo"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
 
-  tags = {
-    Project = "Tech Challenge 2"
-  }
-}
+
+
+
+
+
+
+
+
+  
+
+
+
+
 
 
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,8 +6,18 @@ output "eks_cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.tech_challenge_repo.repository_url
+output "eks_cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
 }
+
+output "eks_cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+/* output "node_group_role_arn" {
+  value = module.eks.eks_managed_node_groups["default"].iam_role_arn
+} */
+
+
 
 
